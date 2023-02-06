67efa7182240e10d7a3a66f5659c70640bdf52f0
Crowdbotics <team@crowdbotics.com>
2023-02-06 16:18:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-mountain-38741",
+  url: "https://polished-mountain-38741.botics.co",
+  api: "https://polished-mountain-38741.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
